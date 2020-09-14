3b5bde7f877e86ac461c03638226c74b7a21c3d6
matthewmjm <matthewmjm@yahoo.com>
2020-09-14 13:55:43 -0600

    add CREATE and DELETE actions to the controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,20 +2,28 @@ class TodosController < ApplicationController
 
     def index
         @todos = Todo.all
-
         render json: @todos
     end
 
-    # def show
-    #     @todo = Todo.find(params[:id])
-    #     render json: @todo
-    # end
+    def show
+        @todo = Todo.find(params[:id])
+        render json: @todo
+    end
 
-    # def create
-    #     @todo = Todo.create(
-    #         title: (params[:title])
-    #     )
-    #     render json: @todo
-    # end
+    def create
+        @todo = Todo.create(
+            title: params[:title],
+            content: params[:content],
+            urgent: params[:urgent],
+            done: params[:done]
+        )
+        render json: @todo, status: :created
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        render status: :no_content
+    end
 
 end
